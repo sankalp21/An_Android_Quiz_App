36fb96614cedd8ed97189a7020533639aaff7064
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-01-28 02:20:09 +0100

    Initial commit

---
+++ app/src/main/java/com/myafrica/africaquiz/MainActivity.java
@@ -0,0 +1,19 @@
+package com.myafrica.africaquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout  LayoutView =(RelativeLayout) findViewById(R.id.FirstQuestion) ;
+    }
+}
